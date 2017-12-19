d910749f3e795616497b40a3c3c7065799c30e7a
Doug Cooper <douglas.cooper@qmul.ac.uk>
2017-12-19 10:27:13 +0000

    Update to version 2017052300

--- version.php
+++ version.php
@@ -10,7 +10,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015120800;
+$plugin->version   = 2017052300;
 $plugin->requires  = 2013051408;
 $plugin->cron      = 0;
 $plugin->component = 'local_pearson';
